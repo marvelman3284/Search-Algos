b487c5924edeb4592264a4caea99dcf860d03e18
marvelman3284 <jedimaster3284@gmail.com>
2022-01-24 08:14:08 -0500

    search

--- src/shah/jeevan/LinearSearch.java
+++ src/shah/jeevan/LinearSearch.java
@@ -1,14 +1,32 @@
 package shah.jeevan;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class LinearSearch {
+
+    public static void main(String[] args)
+    {
+        System.out.print("\f");
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Scanner input=new Scanner(System.in);
+        System.out.println("What number would you like to search for?");
+        int search=input.nextInt();
+        search(array,search);
+    }
+
     public static void search(ArrayList<Integer> lst, Integer findVal) {
+        boolean found = false;
         for (int i : lst) {
             if (i == findVal) {
                 System.out.println("Value " + findVal + " found at index " + lst.indexOf(i));
+                found = true;
                 break;
             }
+        }
+        if (!found) {
+            System.out.println("Value " + findVal + " not found in list.");
         }
     }
 }
